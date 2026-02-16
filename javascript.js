4af2c768aa79b8437955c97f51601b590d9b3b15
Soso16-collab <240217108+Soso16-collab@users.noreply.github.com>
2026-02-16 11:27:33 -0500

    Add custom grid generation and visible borders in the grid

--- javascript.js
+++ javascript.js
@@ -1,19 +1,27 @@
 const sketchContainer = document.querySelector("#sketch-container")
+const dimensionInput = document.querySelector("#dimension-input")
 
 function generateSketchGrid(size) {
     const totalGridCount = size * size
     console.log(totalGridCount)
     for (let i = 0; i < totalGridCount; i++) {
     const sketchDiv = document.createElement("div")
+    sketchDiv.classList.add("sketch-div")
     sketchDiv.style.boxSizing = "border-box"
     sketchDiv.style.width = `${(600/size)}px`
     sketchDiv.style.height = `${600/size}px`
     sketchDiv.style.backgroundColor = "#bcbdc6"
+    sketchDiv.style.border = "2px solid black"
     sketchDiv.addEventListener("mouseenter", () => {
         sketchDiv.style.backgroundColor = "black"
     })
     sketchContainer.appendChild(sketchDiv)
     }
 }
+
+dimensionInput.addEventListener("input", event => {
+    sketchContainer.replaceChildren()
+    generateSketchGrid(event.target.value)
+})
 
 generateSketchGrid(16)
